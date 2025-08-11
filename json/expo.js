2c8269e3a9a3b42566e475aaa50fda962b84f9f5
Sam-78-svg <kendresangamwaman@gmail.com>
2025-08-11 22:54:34 +0530

    commit

---
+++ json/expo.js
@@ -0,0 +1,6 @@
+
+let newObj = '{"name":"sangam","age":20,"isMarid":false,"educatin":{"ssc":"At vk vidyalaya","hsc":"shivaji mahavidyalaya udgir","graduation":{"CGPA":8.75,"Name":"MUMU","Percentage":"76.44%"}},"LaptopName":"Dell Latitude 3400"}';
+
+const exp = JSON.parse(newObj);
+
+export default exp;
